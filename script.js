0ca2bd7c3dc7e210b3b2e21ebac910401e3ae4a3
José Luis ELizondo <elizondostudios@gmail.com>
2023-12-20 18:01:37 -0600

    diseño y funcionalidad

--- script.js
+++ script.js
@@ -2,96 +2,123 @@ function load(){
     fetch("./json/terreno1.json")
     .then(res=> res.json())
     .then(data=>{
-        // body
-        const body= document.querySelector("body")
+        crearMapa(data, ".container")
+    })
+}
 
-        // modal
-        const modal= document.querySelector("#modal")
-        const botonCerrar= document.querySelector("#boton-cerrar")
-        botonCerrar.addEventListener("click", () => {
-            modal.close()
-        })
+function crearMapa(data, container_selector){
+    // contenedor
+    const container= document.querySelector(container_selector)
+    container.style="position: relative;"
 
-        // contenedor
-        const container= document.createElement("div")
-        let containerWidth= 90
-        container.style=`max-width: ${containerWidth}vw; min-width: ${containerWidth}vw;
+    // wrapper
+    const wrapper= document.createElement("div")
+    wrapper.classList.add("wrapper") 
+    wrapper.classList.add("perfect-scroll-custom") 
+    container.appendChild(wrapper)
+
+    // mapa
+    const mapa= document.createElement("div")
+    let mapWidth= 90
+    mapa.style=`max-width: ${mapWidth}%; min-width: ${mapWidth}%;
+        height: auto;
+        background-image: url('${data.background_image}');
+        transition: all ease-out 0.2s;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;`
+
+    wrapper.appendChild(mapa)
+
+    //botones zoom
+    const botonZoomIn= document.createElement("button")
+    botonZoomIn.innerHTML= '<i class="fa-solid fa-magnifying-glass-plus"></i>'
+    botonZoomIn.classList.add("boton-zoom")
+    botonZoomIn.classList.add("boton-zoom-in")
+    botonZoomIn.addEventListener("click", () => {
+        mapWidth+=10
+        mapa.style=`max-width: ${mapWidth}%; min-width: ${mapWidth}%;
             height: auto;
             background-image: url('${data.background_image}');
+            transition: all ease-out 0.2s;
             background-position: center;
             background-repeat: no-repeat;
             background-size: contain;`
-
-        body.appendChild(container)
-
-        //botones zoom
-        const botonZoomIn= document.createElement("button")
-        botonZoomIn.innerHTML= "+"
-        botonZoomIn.classList.add("boton-zoom")
-        botonZoomIn.classList.add("boton-zoom-in")
-        botonZoomIn.addEventListener("click", () => {
-            containerWidth+=10
-            container.style=`max-width: ${containerWidth}vw; min-width: ${containerWidth}vw;
-                height: auto;
-                background-image: url('${data.background_image}');
-                background-position: center;
-                background-repeat: no-repeat;
-                background-size: contain;`
-        })
-        container.appendChild(botonZoomIn)
-
-        const botonZoomOut= document.createElement("button")
-        botonZoomOut.innerHTML= "-"
-        botonZoomOut.classList.add("boton-zoom")
-        botonZoomOut.classList.add("boton-zoom-out")
-        botonZoomOut.addEventListener("click", () => {
-            containerWidth-=10
-            container.style=`max-width: ${containerWidth}vw; min-width: ${containerWidth}vw;
-                height: auto;
-                background-image: url('${data.background_image}');
-                background-position: center;
-                background-repeat: no-repeat;
-                background-size: contain;`
-        })
-        container.appendChild(botonZoomOut)
-
-        // svg
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-        svg.setAttribute("viewBox", `0 0 ${data.background_width} ${data.background_height}`)
-        svg.setAttribute("preserveAspectRatio", "xMidYMid meet")
-        svg.style=`width: 100%;
-            height: 100%;`
-        container.appendChild(svg)
-
-        // poligonos
-        data.polygons.forEach(poly => {
-            const polyElement= document.createElementNS("http://www.w3.org/2000/svg", "polygon")
-            polyElement.classList.add(poly.class)
-            if(poly["stroke"]){
-                polyElement.setAttribute("stroke", poly["stroke"])
-            }
-            if(poly["fill"]){
-                polyElement.setAttribute("fill", poly["fill"])
-            }
-            polyElement.setAttribute("points", poly["points"])
-            // Click event
-            polyElement.addEventListener("click", () => {
-                modal.showModal()
-            })
-
-            // tooltip
-            polyElement.addEventListener("mousemove", (e) => {
-                const tooltip= document.querySelector(".tooltip")
-                tooltip.innerHTML= poly["tooltip"]
-                tooltip.style=`visibility: visible; top: ${e.pageY-40}px; left: ${e.pageX+30}px;`
-            })
-
-            polyElement.addEventListener("mouseout", () => {
-                const tooltip= document.querySelector(".tooltip")
-                tooltip.style=`visibility: hidden;`
-            })
-
-            svg.appendChild(polyElement)
-        });
     })
+    container.appendChild(botonZoomIn)
+
+    const botonZoomOut= document.createElement("button")
+    botonZoomOut.innerHTML= '<i class="fa-solid fa-magnifying-glass-minus"></i>'
+    botonZoomOut.classList.add("boton-zoom")
+    botonZoomOut.classList.add("boton-zoom-out")
+    botonZoomOut.addEventListener("click", () => {
+        mapWidth-=10
+        mapa.style=`max-width: ${mapWidth}%; min-width: ${mapWidth}%;
+            height: auto;
+            background-image: url('${data.background_image}');
+            transition: all ease-out 0.2s;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;`
+    })
+    container.appendChild(botonZoomOut)
+
+    const botonZoomReset= document.createElement("button")
+    botonZoomReset.innerHTML= '<i class="fa-solid fa-arrows-to-circle"></i>'
+    botonZoomReset.classList.add("boton-zoom")
+    botonZoomReset.classList.add("boton-zoom-reset")
+    botonZoomReset.addEventListener("click", () => {
+        mapWidth=90
+        mapa.style=`max-width: ${mapWidth}%; min-width: ${mapWidth}%;
+            height: auto;
+            background-image: url('${data.background_image}');
+            transition: all ease-out 0.2s;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;`
+    })
+    container.appendChild(botonZoomReset)
+
+    // svg
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    svg.setAttribute("viewBox", `0 0 ${data.background_width} ${data.background_height}`)
+    svg.setAttribute("preserveAspectRatio", "xMidYMid meet")
+    svg.style=`width: 100%;
+        height: 100%;`
+    mapa.appendChild(svg)
+
+    // tooltip
+    const tooltip= document.createElement("span")
+    tooltip.classList.add("tooltip")
+    container.appendChild(tooltip)
+
+    // poligonos
+    data.polygons.forEach(poly => {
+        const polyElement= document.createElementNS("http://www.w3.org/2000/svg", "polygon")
+        polyElement.classList.add(poly.class)
+        if(poly["stroke"]){
+            polyElement.setAttribute("stroke", poly["stroke"])
+        }
+        if(poly["fill"]){
+            polyElement.setAttribute("fill", poly["fill"])
+        }
+        polyElement.setAttribute("points", poly["points"])
+
+        // Click event
+        // polyElement.addEventListener("click", () => {
+        //     // Hacer algo
+        // })
+    
+        polyElement.addEventListener("mousemove", (e) => {
+            tooltip.innerHTML= poly["tooltip"]
+            tooltip.style=`visibility: visible; top: ${e.pageY}px; left: ${e.pageX}px;`
+        })
+
+        polyElement.addEventListener("mouseout", () => {
+            const tooltip= document.querySelector(".tooltip")
+            tooltip.style=`visibility: hidden;`
+        })
+
+        svg.appendChild(polyElement)
+    });
 }
